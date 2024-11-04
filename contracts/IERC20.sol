aca01783525e39b045e9505493eccdef77940c9c
lckjcnWq <18819052272@163.com>
2024-11-04 17:49:50 +0800

    feat 1.添加部分IERC20合约定义

---
+++ contracts/IERC20.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.10;
+
+interface IERC20 {
+    //当代币从一个账户转移到另外一个账户
+    event Transfer(address indexed from ,address indexed to ,uint256 value);
+    //当一个账户授权另一个账户使用其代币时触发
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    //返回代币的总供应量
+    function totalSupply() external view returns (uint256);
+
+    //返回指定账户的代币余额
+    function balanceOf(address account) external view returns (uint256);
+
+    //向指定地址转账代币
+    function trasfer(address account, uint256 value) external returns (uint256);
+
+    //返回授权额度
+    function allowance(address owner, address spender) external view returns (uint256);
+    
+    //授权指定地址
+}
