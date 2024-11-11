951bd8beb85572f24cdc30e0df2f447ad8a85050
lukjcnWq <18819052272@163.com>
2024-11-11 21:16:30 +0800

    git commit

--- contracts/pair/IUniswapV2Pair.sol
+++ contracts/pair/IUniswapV2Pair.sol
@@ -18,4 +18,30 @@ interface IUniswapV2Pair {
      * @dev 获取代币1地址
      */
     function token1() external view returns (address);
+
+    //事件定义
+    event Mint(address indexed sender, uint256 amount0, uint256 amount1);
+    event Burn(address indexed sender, uint256 amount0, uint256 amount1, address indexed to);
+    event Swap(address indexed sender, uint256 amount0In, uint256 amount1In, uint256 amount0Out, uint256 amount1Out, address indexed to);
+    event Sync(uint256 reserve0, uint256 reserve1);
+
+    //常用函数
+    function mint(address to) external returns (uint256 liquidity);
+    function burn(address to) external returns (uint256 amount0, uint256 amount1);
+    function swap(uint256 amount0Out, uint256 amount1Out, address to, bytes calldata data) external;
+    function sync() external;
+    function skim(address to) external;
+
+    //获取储备量    
+    function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
+    //获取价格
+    function price0CumulativeLast() external view returns (uint256);
+    function price1CumulativeLast() external view returns (uint256);
+    //获取K值
+    function kLast() external view returns (uint256);
+
+    //获取最小流动性
+    function MINIMUM_LIQUIDITY() external pure returns (uint256);
+    //获取工厂地址
+    function factory() external view returns (address); 
 }
---
+++ contracts/pair/UniswapV2ERC20.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+import "../tokens/ERC20Basic.sol";
+
+contract UniswapV2ERC20 is ERC20Basic {
+
+}
